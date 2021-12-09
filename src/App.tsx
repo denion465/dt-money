75bb7a49722dd8b8ba441e0fead21f51886d9dee
Daniel Vidal <danielsv465@hotmail.com>
2021-12-09 20:46:53 -0300

    Criando modal de transação e estilizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransaction() {
+    setIsNewTransactionModal(true);
+  };
+
+  function handleCloseNewTransaction() {
+    setIsNewTransactionModal(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransaction} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransaction}
+      />
       <GlobalStyle />
     </>
   );
